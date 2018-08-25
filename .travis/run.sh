f23515d66b4b0d8fc538beff63b844212a78193f
LSmith-Zenoscave <lsmith@zenoscave.com>
2018-08-25 16:10:45 -0600

    simplify requirements. add global set -e
    Signed-off-by: LSmith-Zenoscave <lsmith@zenoscave.com>

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# make sure failing tests cause failing
 export PATH="${PATH}:${HOME}/.local/bin"
 pushd $(dirname $0)/..
+
+# make sure failing tests cause failing
+set -e
 for part in smoke-editor smoke-backend smokr; do
     pushd ${part}
-    set -e
     [ -f .travis/test.sh ] && .travis/test.sh
-    set +e
     popd
 done
+set +e
+
 popd
