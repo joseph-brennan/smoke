0543c4e2d362668a0c256ae1e47a6e5d0cd57f87
Marlon Lopez <mlopez98@msudenver.edu>
2018-09-13 21:14:53 -0600

    revised comments

--- smoke-backend/smoke_backend/configtest.py
+++ smoke-backend/smoke_backend/configtest.py
@@ -1,6 +1,5 @@
-
+# -*- coding: utf-8 -*-
 """ Tells how to connect to the database
 """
-# -*- coding: utf-8 -*-
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
--- smoke-backend/smoke_backend/wsgi.py
+++ smoke-backend/smoke_backend/wsgi.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 """Creates an instance of the app that can be imported
 """
-# -*- coding: utf-8 -*-
 
 from smoke_backend.app import create_app
 
