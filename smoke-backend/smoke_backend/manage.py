fb759bb97dd12305c802b2f8816d3b55defa8640
Michael Newman <newms226@gmail.com>
2018-09-18 09:29:49 -0600

    Updated to Lukes standards

--- smoke-backend/smoke_backend/manage.py
+++ smoke-backend/smoke_backend/manage.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
+""" This file manages the database of users, the creation of users,
+and the initialization of both for the backend of smokr. """
 
-''' Command Line Tools '''
+
 import click 
-''' Flask Command Line Tools'''
 from flask.cli import FlaskGroup
-''' Import application factory method '''
 from smoke_backend.app import create_app
 
 
 def create_smoke(info):
-    ''' Get application from application factory method '''
+    """ Get application from application factory method """
     return create_app(cli=True)
 
 
@@ -23,18 +23,14 @@ def init():
     """Init application, create database tables
     and create a new user named admin with password admin
     """
-    
-    '''' Import the database of User objects through SQLAlchemy '''
+
     from smoke_backend.extensions import db
-    ''' Import the User class '''
     from smoke_backend.models import User
     
-    ''' Form the database tables '''
     click.echo("create database")
     db.create_all()
     click.echo("done")
 
-    ''' Create the default user '''
     click.echo("create user")
     user = User(
         username='admin',
@@ -43,11 +39,11 @@ def init():
         active=True
     )
     
-    ''' Add the user to the database '''
+    
     db.session.add(user)
     db.session.commit()
     click.echo("created user admin")
 
 if __name__ == "__main__":
-    ''' If this method is called as the main method, start client mode '''
+    """ If this method is called as the main method, start client mode """
     cli()
