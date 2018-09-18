f3bf9ed7c95084d52393b288a451510b51a40e7d
tylerlindblad <43221098+tylerlindblad@users.noreply.github.com>
2018-09-17 23:41:49 -0600

    Added comments to pagination.py and __init__.py
    Added comments in what I believe is the correct style to pagination.py and __init__.py in the commons folder

--- smoke-backend/smoke_backend/auth/__init__.py
+++ smoke-backend/smoke_backend/auth/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+"""Make Python treat the directories as containing packages
+"""
 from . import views
 
 __all__ = [
--- smoke-backend/smoke_backend/commons/pagination.py
+++ smoke-backend/smoke_backend/commons/pagination.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 
-"""Simple helper to paginate query
+"""Simple helper to paginate query.
 """
 from flask import url_for, request
 
-"""Sets the standard page size as well as creating the first page"""
 DEFAULT_PAGE_SIZE = 50
 DEFAULT_PAGE_NUMBER = 1
 
-"""Creates the first page"""
 def paginate(query, schema):
+    """A simple helper to paginate a query.
+
+    This function will take a query and schema and separate the query results into pages with 50 entries per page.
+    It will also create links to the next and previous result pages as objects called next and previous.
+    The function returns the total number of pages, the current page a user is on, the next page object, the previous
+    page object, and the results of the current page.
+    """
     page = request.args.get('page', DEFAULT_PAGE_NUMBER)
     per_page = request.args.get('page_size', DEFAULT_PAGE_SIZE)
     page_obj = query.paginate(page=page, per_page=per_page)
 
-    """Creates a next object that will contain data to the next page if there is another page"""
     next = url_for(
         request.endpoint,
         page=page_obj.next_num if page_obj.has_next else page_obj.page,
@@ -22,7 +26,6 @@ def paginate(query, schema):
         **request.view_args
     )
 
-    """Creates a previous object that will contain data to the previous page if there is a previous page"""
     prev = url_for(
         request.endpoint,
         page=page_obj.prev_num if page_obj.has_prev else page_obj.page,
@@ -30,8 +33,6 @@ def paginate(query, schema):
         **request.view_args
     )
 
-    """Returns the total number of pages, the current page a user is on, the next object, the previous object, and
-        the results of the current page"""
     return {
         'total': page_obj.total,
         'pages': page_obj.pages,
