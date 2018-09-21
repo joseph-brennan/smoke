c478cc6bd235a5ac4b724a6094d82d80a5eee32e
Michael Newman <newms226@gmail.com>
2018-09-21 14:23:57 -0600

    init() attempt

--- smoke-backend/docs/conf.py
+++ smoke-backend/docs/conf.py
@@ -33,7 +33,11 @@ import smoke_backend
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.coverage']
+
+# Napolean settings
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
