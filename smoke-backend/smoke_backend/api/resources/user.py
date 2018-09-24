561b8526ec31ffbf2cf8ddabe4ebe892c1c1e12a
Michael Newman <newms226@gmail.com>
2018-09-24 15:35:18 -0600

    users.py documentation update

--- smoke-backend/smoke_backend/api/resources/user.py
+++ smoke-backend/smoke_backend/api/resources/user.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-"""Communicates with database to create, update, or delete users.
-
-Serialization provided by Marshmallow [#f1]_
+"""Handles communication with the SQLAlchemy database and handles its contents.
 
 .. [#f1] https://marshmallow.readthedocs.io/en/3.0/
 .. [#f2] http://docs.sqlalchemy.org/en/latest/orm/session_api.html#sqlalchemy.orm.session.Session
 .. [#f3] https://pythonhosted.org/Flask-JWT/
 .. [#f4] http://docs.sqlalchemy.org/en/latest/core/schema.html
 .. [#f5] https://flask-restful.readthedocs.io/en/0.3.5/quickstart.html
+.. [#f6] https://pythonhosted.org/Flask-JWT/
 """
 from flask import request
 from flask_restful import Resource
@@ -19,30 +18,17 @@ from smoke_backend.commons.pagination import paginate
 
 
 class UserSchema(ma.ModelSchema):
-    """Single object schema
+    """Models a current user logged in to the backend through a single object Marshmallow Model. [#f1]_
 
-    Class attempts to verify a password passed to it by a user
+    Extends the Marshmallow ModelSchema class. [#f1]_
 
     Attributes:
-        password (String): The user entered password
-
-            Serialization, Persistence, & Verification by Marshmallow [#f1]_
-
-    Args:
-        ma.ModelSchema: The SQLAlchemy Schema used by Marshmallow to model the User [#f1]_
-
+        password (String): The user entered password. Serialization, Persistence, & Verification by Marshmallow. [#f1]_
     """
     password = ma.String(load_only=True, required=True)
 
     class Meta:
-        """Metadata of the login session
-
-        Attributes:
-            model (User): The specific SQLAlchemy Schema [#f4]_ which represents the current user
-
-            sqla_session (Session): The SQLAlchemy session object [#f2]_
-
-        """
+        """Metadata of the login session."""
         model = User
         sqla_session = db.session
 
@@ -50,32 +36,41 @@ class UserSchema(ma.ModelSchema):
 class UserResource(Resource):
     """Single object resource.
 
+    Extends a Flask-RESTful Resource [#f4]_ object to direct the control of this class.
+
     Attributes:
         method_decorators: Singleton array of decorator objects to require a valid JWT token to be present. [#f3]_
-
-    Args:
-        Resources: A Flask-RESTful Resource [#f4]_ object to direct the control of this class.
+            [#f6]_
     """
     method_decorators = [jwt_required]
 
     def get(self, user_id):
         """Show and return a user.
 
-        Args:
+        Paramaters:
             user_id (int): The ID of the user to get.
 
         Returns:
+            user: A dictionary of the user data.
 
+        Raises:
+            404: If user was not available.
         """
         schema = UserSchema()
         user = User.query.get_or_404(user_id)
         return {"user": schema.dump(user).data}
 
     def put(self, user_id):
-        """update a user
+        """Update a user.
 
-        Args:
-            user_id (int): The ID of the user to get
+        Paramaters:
+            user_id (int): The ID of the user to get.
+
+        Returns:
+            user: Returns a dictionary of the user data or an error message if the User was not in the database.
+
+        Raises:
+            404: If user was not available.
         """
         schema = UserSchema(partial=True)
         user = User.query.get_or_404(user_id)
@@ -86,10 +81,16 @@ class UserResource(Resource):
         return {"msg": "user updated", "user": schema.dump(user).data}
 
     def delete(self, user_id):
-        """delete a user
+        """Delete a user.
 
-        Args:
-            user_id (int): The ID of the user to get
+        Paramaters:
+            user_id (int): The ID of the user to get.
+
+        Returns:
+            str: A message noting that the user was deleted.
+
+        Raises:
+            404: If user was not available.
         """
         user = User.query.get_or_404(user_id)
         db.session.delete(user)
@@ -99,29 +100,34 @@ class UserResource(Resource):
 
 
 class UserList(Resource):
-    """Creation and get_all
+    """Returns a list of all of the users currently in the database.
+
+    Uses pagination.py to generate a paginated view of all of the users in the database.
 
     Attributes:
-        method_decorators: Array of decorator objects to require a valid JWT token to be present. [#f3]_
-
+        method_decorators: Singleton array of decorator objects to require a valid JWT token to be present. [#f3]_
+            [#f6]_
     """
     method_decorators = [jwt_required]
 
     def get(self):
-        """Get a list of all users
+        """Get a list of all users.
 
         Returns:
-            A paginated list of all the users as defined by pagination.py
+            A paginated list of all the users as defined by pagination.py.
         """
         schema = UserSchema(many=True)
         query = User.query
         return paginate(query, schema)
 
     def post(self):
-        """Create a new user & put it in the database if there are no errors
+        """Create a new user & put it in the database if there are no errors.
 
         Returns:
-            String: Noting whether the user was created or the error which caused creation to fail
+            String: Noting whether the user was created or the error which caused creation to fail.
+
+        Raises:
+            404: If the json request failed.
         """
         schema = UserSchema()
         user, errors = schema.load(request.json)
