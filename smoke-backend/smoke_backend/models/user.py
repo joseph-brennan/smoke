02058ee8a5687970ec0d084904da0405349ffb74
kclark85 <kclark85@msudenver.edu>
2018-09-24 22:18:57 -0600

    changed permission parameter from string to int

--- smoke-backend/smoke_backend/models/user.py
+++ smoke-backend/smoke_backend/models/user.py
@@ -11,7 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     active = db.Column(db.Boolean, default=True)
-    permissions = db.Column(db.String(7), default = "student")
+    permissions = db.Column(db.Integer(1), default=0)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
