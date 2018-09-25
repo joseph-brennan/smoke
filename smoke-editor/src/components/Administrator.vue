88da50eb36a64c400c80f362790cea834d61b5c6
Eryn Kelsey-Adkins <ekelseya@msudenver.edu>
2018-09-25 14:36:05 -0600

    Created Vue components for users
    Created Vue components for each type of user. Components are based on Home.vue and are simplistic frameworks to act as mocks for authentication.

---
+++ smoke-editor/src/components/Administrator.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="Home" class="col-sm-6 col-sm-offset-3">
+    <h1>Welcome, Administrator!</h1>
+  </div>
+</template>
---
+++ smoke-editor/src/components/Student.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="Home" class="col-sm-6 col-sm-offset-3">
+    <h1>Welcome, Student!</h1>
+  </div>
+</template>
