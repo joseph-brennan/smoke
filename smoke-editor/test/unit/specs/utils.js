ef9a6f981c5a3908cad58239403fe5475a41771a
Eryn Kelsey-Adkins <ekelseya@msudenver.edu>
2018-09-25 09:17:18 -0600

    Adding moxios - not tested
    Tried adding moxios
    Not tested

--- smoke-editor/test/unit/specs/utils.js
+++ smoke-editor/test/unit/specs/utils.js
@@ -1,5 +1,25 @@
 import Vue from 'vue'
 import moxios from 'moxios'
+import axios from 'axios'
+import sinon from 'sinon'
+import { equal } from 'assert'
+
+describe('user authentication', function(){
+    beforeEach((function) {
+        moxios.install(http)
+    })
+
+    afterEach(function() {
+        moxios.uninstall(http)
+    })
+    
+    it('fetches the appropriate page for the user', (done) {
+        moxios.stubRequest('localhost:8080/auth/login', {
+            status: 200,
+            responseText: 'Passed Test' 
+            })
+    })
+})
 
 function mount (Component, options) {
   const Constructor = Vue.extend(Component)
