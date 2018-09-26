05e9f42bf7e955673fc4ffcb04e45805eb159d93
Michael Newman <newms226@gmail.com>
2018-09-26 12:10:59 -0600

    Format fix

--- smoke-backend/docs/conf.py
+++ smoke-backend/docs/conf.py
@@ -25,6 +25,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import smoke_backend
 
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
