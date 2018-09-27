a727749c393ec6a1676e28c97c4d0c2a5797055d
RyanFlageolle <rflageol@msudenver.edu>
2018-09-27 11:11:44 -0600

    removed extra blankline

--- smoke-backend/smoke_backend/models/user.py
+++ smoke-backend/smoke_backend/models/user.py
@@ -8,7 +8,6 @@ class User(db.Model):
     """
     __tablename__ = 'users'
 
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
