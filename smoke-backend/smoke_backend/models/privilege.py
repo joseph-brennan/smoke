d8b794acc0bba2d62fa1a4a6b66593108032d0b2
joe <joseph0brennan@gmail.com>
2018-09-27 13:41:45 -0600

    hopefully just fixed it i think the permission needs to be an integer

--- smoke-backend/smoke_backend/models/privilege.py
+++ smoke-backend/smoke_backend/models/privilege.py
@@ -5,7 +5,7 @@ class Privilege(db.Model):
 
     id = db.Column('id', db.Integer, primary_key=True)
     permission_level = db.Column(
-        'permission_level', db.String(8), unique=True)
+        'permission_level', db.Integer, unique=True)
 
     def __init__(self, **kwargs):
         super(Privilege, self).__init__(**kwargs)
