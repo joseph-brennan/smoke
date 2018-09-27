456fbffcd86599656f74f59840e31593b388769a
RyanFlageolle <rflageol@msudenver.edu>
2018-09-27 15:00:37 -0600

    running tests pass

--- smoke-backend/smoke_backend/api/resources/privilege.py
+++ smoke-backend/smoke_backend/api/resources/privilege.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
 
-from smoke_backend.models import Privilege
-from smoke_backend.extensions import ma
-
-
-class PrivilegeSchema(ma.ModelSchema):
-
-    class Meta:
-        model = Privilege
--- smoke-backend/smoke_backend/manage.py
+++ smoke-backend/smoke_backend/manage.py
@@ -13,22 +13,7 @@ def create_smoke(info):
 @click.group(cls=FlaskGroup, create_app=create_smoke)
 def cli():
     """Main entry point"""
-    from smoke_backend.extensions import db
-    from smoke_backend.models import User
-    click.echo("create database")
-    db.create_all()
-    click.echo("done")
-    click.echo("create user")
-    user = User(
-        username='admin',
-        email='admin@mail.com',
-        password='admin',
-        active=True,
-        privilege_id=3
-    )
-    db.session.add(user)
-    db.session.commit()
-    click.echo("created user admin")
+
 
 
 @cli.command("init")
@@ -36,6 +21,37 @@ def init():
     """Init application, create database tables
     and create a new user named admin with password admin
     """
+    from smoke_backend.extensions import db
+    click.echo("create database")
+    db.create_all()
+    click.echo("done")
+
+@cli.command("seed")
+def seed():
+    from smoke_backend.extensions import db
+    from smoke_backend.models import Privilege, User
+
+    privilege1 = Privilege(permission_level="STUDENT")
+    privilege2 = Privilege(permission_level="TEACHER")
+    privilege3 = Privilege(permission_level="ADMIN")
+    db.session.add(privilege1)
+    db.session.add(privilege2)
+    db.session.add(privilege3)
+
+    db.session.commit()
+
+    click.echo("create user")
+    user = User(
+        username='admin',
+        email='admin@mail.com',
+        password='admin',
+        active=True,
+        privilege=Privilege.query.get(3)
+        )
+    db.session.add(user)
+    db.session.commit()
+    click.echo("created user admin")
+
 
 
 if __name__ == "__main__":
