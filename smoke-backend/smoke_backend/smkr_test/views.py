8f41aac6a61074e32148bf09e5998a0e00b263ec
kclark85 <32521814+kclark85@users.noreply.github.com>
2018-10-04 13:16:03 -0600

    tentative framework for views.py

---
+++ smoke-backend/smoke_backend/smkr_test/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Creates view for JSON handling.
+
+Attributes:
+    blueprint (Flask Blueprint): The blueprint scheme for smoke. [fblueprint]_
+"""
+
+from flask import request, jsonify, Blueprint
+
+from smoke_backend.models import User
+from smoke_backend.extensions import pwd_context, jwt
+import subprocess
+
+
+blueprint = Blueprint('JSON', __name__, url_prefix='/JSON')
+
+
+@blueprint.route('/test', methods=['POST'])
+def stringifyJSON():
+    subprocess.call(['./run_smkr.sh'])
+    
+
