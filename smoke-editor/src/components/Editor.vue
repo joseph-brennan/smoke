2e0f377ffc0dcc403d0892dac6ca1dab44ed2cce
Andrew P <masterbahamut07@gmail.com>
2018-10-04 09:46:00 -0600

    Changed to Bulma-compliant dropdown

--- smoke-editor/src/components/Editor.vue
+++ smoke-editor/src/components/Editor.vue
@@ -1,12 +1,29 @@
 <template>
   <div id = "editor">
-    <div class="dropdown">
-      <button @click = "myFunction()" class = "dropbtn">Dropdown</button>
-      <div id='myDropdown' class="dropdown-content">
-        <a href="#">testing</a>
-        <a href="#">testing2</a>
-        <a href="#">testing3</a>
-        <a href="#">testing4</a>
+    <div class="dropdown is-active">
+      <div class="dropdown-trigger">
+        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+          <span>Editor Mode</span>
+          <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+        </button>
+      </div>
+      <div class="dropdown-menu" id="dropdown-menu" role="menu">
+        <div class="dropdown-content">
+          <a href="#" class="dropdown-item">
+            Python 2
+          </a>
+          <a class="dropdown-item">
+            Python 3
+          </a>
+          <a href="#" class="dropdown-item">
+            Ruby
+          </a>
+          <a href="#" class="dropdown-item">
+            Javascript
+          </a>
+        </div>
       </div>
     </div>
   <div>
@@ -23,56 +40,6 @@
   </div>
 </template>
 
-// ************************************************************************
-<style>
-  /* Dropdown button */
-  .dropbtn {
-    background-color: #333;
-    color: white;
-    padding: 3px;
-    font-size: 14px;
-    border: none;
-    cursor: pointer;
-  }
-
-  /* Dropdown button on hover & focus */
-  .dropbtn:hover, .dropbtn:focus {
-    background-color: #2980B9;
-  }
-
-  /* The container <div> - needed to position the dropdown content */
-  .dropdown {
-    position: relative;
-    top: 0px;
-    left: 40px;
-    display: inline-block;
-  }
-
-  /* Dropdown Content (Hidden by Default) */
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f1f1f1;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-
-  /* Links inside the dropdown */
-  .dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-  }
-
-  /* Change color of dropdown links on hover */
-  .dropdown-content a:hover {background-color: #ddd}
-
-  /* Show the dropdown menu (use JS to add this class to the .dropdown-content container when the user clicks on the dropdown button) */
-  .show {display:block;}
-</style>
-// ****************************************************************
 <script>
 import Brace from 'vue-bulma-brace'
 
