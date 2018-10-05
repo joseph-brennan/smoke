7c37b5b491b1b436ac146d8c3793e20d9972807b
Ryan Harper <noyh.rynban@gmail.com>
2018-10-05 09:59:53 -0600

    Removed trailing space to satisfy eslint

--- smoke-editor/src/components/SubmitButton.vue
+++ smoke-editor/src/components/SubmitButton.vue
@@ -10,7 +10,7 @@ export default {
     clicked: function () {
       console.log('i was clicked!!!')
       console.log(this.editorCode)
-      // We can base64 encode this.editorCode and 
+      // We can base64 encode this.editorCode and
       // send it to the API URL via an AJAX call
     }
   },
