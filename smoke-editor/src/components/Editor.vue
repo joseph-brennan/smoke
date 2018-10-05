575b2875df917e75047962ccd77ffeded21e3b45
Andrew P <masterbahamut07@gmail.com>
2018-10-04 19:30:12 -0600

    Switched to Buefy; Added mode variable

--- smoke-editor/src/components/Editor.vue
+++ smoke-editor/src/components/Editor.vue
@@ -1,69 +1,68 @@
 <template>
-  <div id = "editor">
-    <div class="dropdown is-active">
-      <div class="dropdown-trigger">
-        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-          <span>Editor Mode</span>
-          <span class="icon is-small">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-        </button>
-      </div>
-      <div class="dropdown-menu" id="dropdown-menu" role="menu">
-        <div class="dropdown-content">
-          <a href="#" class="dropdown-item">
-            Python 2
-          </a>
-          <a class="dropdown-item">
-            Python 3
-          </a>
-          <a href="#" class="dropdown-item">
-            Ruby
-          </a>
-          <a href="#" class="dropdown-item">
-            Javascript
-          </a>
+  <div class = "editor">
+    <b-dropdown v-model="mode">
+      <button class="button is-primary" type="button" slot="trigger">
+        <template v-if="'Python 3'">
+          <span>Python 3</span>
+        </template>
+        <template v-else-if="'Python 2'">
+          <span>Python 2</span>
+        </template>
+      </button>
+
+      <b-dropdown-item :value="'Python 3'">
+        <div class="dropdown-content" @click="changeMode('Python 3')">
+          <div class="dropdown-item">
+            <h3>Python 3</h3>
+          </div>
         </div>
-      </div>
-    </div>
-  <div>
+      </b-dropdown-item>
+
+      <b-dropdown-item :value="'Python 2'">
+        <div class="dropdown-content" @click="changeMode('Python 2')">
+          <div class="dropdown-item">
+            <h3>Python 2</h3>
+          </div>
+        </div>
+      </b-dropdown-item>
+    </b-dropdown>
+
     <brace style="height: 500px"
-      :fontsize="'12px'"
-      :theme="'github'"
-      :mode="'python'"
-      :codefolding="'markbegin'"
-      :softwrap="'free'"
-      :selectionstyle="'text'"
-      :highlightline="true">
+           :fontsize="'12px'"
+           :theme="'github'"
+           :mode="'python'"
+           :codefolding="'markbegin'"
+           :softwrap="'free'"
+           :selectionstyle="'text'"
+           :highlightline="true">
     </brace>
-  </div>
   </div>
 </template>
 
+
+
 <script>
 import Brace from 'vue-bulma-brace'
+import Vue from 'vue'
+import Buefy from 'buefy'
+// import 'buefy/lib/buefy.css'
 
-export default {
+Vue.use(Buefy)
+
+export default{
+  data () {
+    return {
+      mode: 'Python 3'
+    }
+  },
   components: {
     Brace
   },
   methods: {
-    myFunction: function () {
-      document.getElementById('myDropdown').classList.toggle('show')
-    }
-  }
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content')
-    var i
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show')
-      }
+    changeMode (newMode) {
+      console.log(this.mode)
+      this.mode = newMode
+      console.log(this.mode)
     }
   }
 }
