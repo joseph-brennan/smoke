9cc9b30341d545da74189b9a08a8014f49c22a5a
DKL <dyip@msudenver.edu>
2018-10-06 18:09:27 -0600

    Encode object and working on getting jquery to work

--- smoke-editor/src/components/SubmitButton.vue
+++ smoke-editor/src/components/SubmitButton.vue
@@ -5,6 +5,10 @@
 </template>
 
 <script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+
 export default {
   methods: {
     clicked: function () {
@@ -12,6 +16,17 @@ export default {
       console.log(this.editorCode)
       // We can base64 encode this.editorCode and
       // send it to the API URL via an AJAX call
+
+      var encodedObject = window.btoa(this.editorCode)
+
+      $(document).ready(function () {
+        $('button').click(function () {
+          $.post('API_v1_test',
+            {
+              data: encodedObject
+            })
+        })
+      })
     }
   },
   props: ['editorCode']
