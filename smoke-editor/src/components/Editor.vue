78156bd3d7015eedef7cd68a55036cd9152d1203
Peter Nielson <computergeekpete@gmail.com>
2018-10-07 21:51:14 -0600

    Removed previous html

--- smoke-editor/src/components/Editor.vue
+++ smoke-editor/src/components/Editor.vue
@@ -1,41 +1,5 @@
 <template>
   <div class = "editor">
-    <b-dropdown v-model="mode">
-      <button class="button is-primary" type="button" slot="trigger">
-        <template v-if="'Python 3'">
-          <span>Python 3</span>
-        </template>
-        <template v-else-if="'Python 2'">
-          <span>Python 2</span>
-        </template>
-      </button>
-
-      <b-dropdown-item :value="'Python 3'">
-        <div class="dropdown-content" @click="changeMode('Python 3')">
-          <div class="dropdown-item">
-            <h3>Python 3</h3>
-          </div>
-        </div>
-      </b-dropdown-item>
-
-      <b-dropdown-item :value="'Python 2'">
-        <div class="dropdown-content" @click="changeMode('Python 2')">
-          <div class="dropdown-item">
-            <h3>Python 2</h3>
-          </div>
-        </div>
-      </b-dropdown-item>
-    </b-dropdown>
-
-    <brace style="height: 500px"
-           :fontsize="'12px'"
-           :theme="'github'"
-           :mode="'python'"
-           :codefolding="'markbegin'"
-           :softwrap="'free'"
-           :selectionstyle="'text'"
-           :highlightline="true">
-    </brace>
   </div>
 </template>
 
