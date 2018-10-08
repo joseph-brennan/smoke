711dbf66799ae4bca00a8935371c175182bdb60b
Ryan Harper <noyh.rynban@gmail.com>
2018-10-08 11:07:25 -0600

    Removed debug lines and made minor changes, added leading slash to URL

--- smoke-editor/src/components/SubmitButton.vue
+++ smoke-editor/src/components/SubmitButton.vue
@@ -5,21 +5,13 @@
 </template>
 
 <script>
-
 import axios from 'axios'
 
 export default {
   methods: {
     clicked: function () {
-      console.log('i was clicked!!!')
-      console.log(this.editorCode)
-      // We can base64 encode this.editorCode and
-      // send it to the API URL via an AJAX call
-
-      var encodedObject = window.btoa(this.editorCode)
-
-      axios.post('API/v1/test', {
-        data: encodedObject
+      axios.post('/API/v1/test', {
+        data: window.btoa(this.editorCode)
       })
     }
   },
