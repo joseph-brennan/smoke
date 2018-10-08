5cc71d72f3b0efe65f87704b733403d3998b1929
Peter Nielson <computergeekpete@gmail.com>
2018-10-07 22:14:19 -0600

    Working drop-down and language selection with default

--- smoke-editor/src/components/Editor.vue
+++ smoke-editor/src/components/Editor.vue
@@ -7,9 +7,8 @@
       </button>
 
       <b-dropdown-item v-for="lang in languages"
-                       v-bind:data="lang"
-                       v-bind:key="lang.name"
-                       v-bind:value="lang.value">
+                       v-bind:value="lang"
+                       v-bind:key="lang.name">
                        {{ lang.name }}
       </b-dropdown-item>
     </b-dropdown>
@@ -29,8 +28,8 @@
 
 
 <script>
-import Brace from 'vue-bulma-brace'
 import Vue from 'vue'
+import Brace from 'vue-bulma-brace'
 import Buefy from 'buefy'
 // import 'buefy/lib/buefy.css'
 
@@ -41,38 +40,38 @@ export default{
     Brace
   },
   data: function () {
-      let languages: [
-        {
-          name: 'Python 3',
-          tag: 'python'
-        },
-        {
-          name: 'Python 2',
-          tag: 'python'
-        },
-        {
-          name: 'Ruby',
-          tag: 'ruby'
-        },
-        {
-          name: 'Java',
-          tag: 'java'
-        },
-        {
-          name: 'JavaScript',
-          tag: 'javascript'
-        },
-        {
-          name: 'C++',
-          tag: 'cpp'
-        },
-        {
-          name: 'Erlang',
-          tag: 'erlang'
-        }
-      ]
+    let languages = [
+      {
+        name: 'Python 3',
+        tag: 'python'
+      },
+      {
+        name: 'Python 2',
+        tag: 'python'
+      },
+      {
+        name: 'Ruby',
+        tag: 'ruby'
+      },
+      {
+        name: 'Java',
+        tag: 'java'
+      },
+      {
+        name: 'JavaScript',
+        tag: 'javascript'
+      },
+      {
+        name: 'C++',
+        tag: 'cpp'
+      },
+      {
+        name: 'Erlang',
+        tag: 'erlang'
+      }
+    ]
     return {
-      languages: languages
+      languages: languages,
       selectedLanguage: languages[0]
     }
   }
