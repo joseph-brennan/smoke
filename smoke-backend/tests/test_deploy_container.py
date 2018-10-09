cbb8392611cf0ac1c75225413b033bbb1d90ff55
kclark85 <32521814+kclark85@users.noreply.github.com>
2018-10-09 11:57:19 -0600

    removed extraneous methods

--- smoke-backend/tests/test_deploy_container.py
+++ smoke-backend/tests/test_deploy_container.py
@@ -4,25 +4,7 @@ import subprocess
 import unittest
 import json
 def mocked_requests_get(*args,**kwargs):
-        class MockResponse:
-            def __init__(self, json_data, status_code):
-                self.json_data = json_data
-                self.status_code = status_code
-                self.json_string = json.dumps(json_data)
 
-            def json(self):
-                
-                return self.json_data
-       # mock the responses to each argument to be used in the test
-        if args[0] == 'http://someurl.com/test.json':
-            return MockResponse({"key1": "value1"}, 200)
-        elif args[0] == 'http://someotherurl.com/anothertest.json':
-            return MockResponse({"key2": "value2"},200)
-
-        return MockResponse(None, 404)
-    #TODO: mock the stringifyJSON method (get stringify info from Ron)
-
-class JSONTesterTest(unittest.TestCase):
      def test_stringify(self):
         rep = client.post(
               '/JSON/test',
@@ -30,9 +12,5 @@ class JSONTesterTest(unittest.TestCase):
               headers={'content-type': 'application/json'}
               )
         
-        assert rep.status_code == 400
-        assert rep.stringify_json()['msg'] == {'Bryan': 'Blageolle'}
+        assert stringify_json() == {'Bryan': 'Blageolle'}
         
-
-if __name__ == '__main__':
-    unittest.main()
