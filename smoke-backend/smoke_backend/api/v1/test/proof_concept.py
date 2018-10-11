659471ba2002517df3813e6866004f25b2a15e08
joe <joseph0brennan@gmail.com>
2018-10-11 16:11:48 -0600

    bouncing between my machines

--- smoke-backend/smoke_backend/api/v1/test/proof_concept.py
+++ smoke-backend/smoke_backend/api/v1/test/proof_concept.py
@@ -6,8 +6,9 @@ def this_works():
     client = docker.from_env()
 
     client.images.build(path='.', tag="alpine:test")
+    test = "hello world"
 
-    print(client.containers.run(image="alpine:test", auto_remove=True))
+    print(client.containers.run(image="alpine:test", auto_remove=True, environment=["STRING={}".formtat(test)]))
     # print(client.containers.run("alpine", ["echo", "hello world"]))
 
 
