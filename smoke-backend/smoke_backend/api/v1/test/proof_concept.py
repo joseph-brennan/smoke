b94f141ea686ff0087b09d9ee98dcfeb267486c4
Joey <sharpshooter30@gmail.com>
2018-10-11 15:34:31 -0600

    got the stand alone working

--- smoke-backend/smoke_backend/api/v1/test/proof_concept.py
+++ smoke-backend/smoke_backend/api/v1/test/proof_concept.py
@@ -3,7 +3,9 @@ import docker
 
 def this_works():
     client = docker.from_env()
-    print(client.containers.run("alpine", ["echo", "hello world"]))
+    client.images.build(path='.', tag="alpine:test")
+    print(client.containers.run(image="alpine:test", auto_remove=True))
+    # print(client.containers.run("alpine", ["echo", "hello world"]))
 
 
 this_works()
