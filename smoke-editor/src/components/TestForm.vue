d0729ce2dd53c957cc8f8cf71d7cf3d378b86a64
RyanFlageolle <rflageol@msudenver.edu>
2018-10-25 14:01:30 -0600

    finished

---
+++ smoke-editor/src/components/TestForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <h1 class="title is-primary">New Test Form</h1>
+
+      <div class="form-group ">
+        <label class="label">Test Name</label>
+        <input clss="text" placeholder="Test Name"/>
+      </div>
+    </div>
+  </div>
+</template>
