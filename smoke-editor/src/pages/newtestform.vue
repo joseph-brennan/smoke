1bc9b7eb25532afcf26b71e58f81ddd5dbfc62a0
RyanFlageolle <rflageol@msudenver.edu>
2018-10-31 15:39:15 -0600

    completed the form

--- smoke-editor/src/pages/newtestform.vue
+++ smoke-editor/src/pages/newtestform.vue
@@ -19,6 +19,28 @@
       <b-field label="Constraints*">
         <b-input v-model="test.constraints" type="textarea"/>
       </b-field>
+      <br>
+      <label class="title">Test Cases*</label>
+      <br>
+      <div class="columns" label="Test 1">
+        <div class="column">
+          <textarea class="textarea" rows="5" placeholder="Input"></textarea>
+           </div>
+           <div class="column">
+             <textarea class="textarea" rows="5" placeholder="Output"></textarea>
+           </div>
+         </div>
+       </div>
+       <div class="columns">
+         <div class="column" label="Validator 1">
+           <textarea class="textarea" rows="5" placeholder="Input"></textarea>
+            </div>
+            <div class="column">
+              <textarea class="textarea" rows="5" placeholder="Output"></textarea>
+            </div>
+          </div>
+        </div>
+
       <button
         id="submit"
         class="button"
